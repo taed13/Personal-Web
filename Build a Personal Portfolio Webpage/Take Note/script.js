b8feefd3dbdb1b7350dfab85ed6367f207a44744
taed13 <tiendatpot@gmail.com>
2023-03-04 17:49:53 +0700

    done showing all notes from local storage

---
+++ Build a Personal Portfolio Webpage/Take Note/script.js
@@ -0,0 +1,65 @@
+const addBox = document.querySelector(".add-box"),
+    popupBox = document.querySelector(".popup-box"),
+    closeIcon = popupBox.querySelector("header i"),
+    titleTag = popupBox.querySelector("input"),
+    descTag = popupBox.querySelector("textarea"),
+    addBtn = popupBox.querySelector("button");
+
+const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+addBox.addEventListener("click", () => {
+    popupBox.classList.add("show");
+});
+
+closeIcon.addEventListener("click", () => {
+    titleTag.value = "";
+    descTag.value = "";
+    popupBox.classList.remove("show");
+});
+
+function showNotes() {
+    document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note) => {
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${note.description}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span> 
+                            <div class="settings">
+                                <i class="uil uil-ellipsis-h"></i>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let noteTitle = titleTag.value,
+        noteDesc = descTag.value;
+
+    if (noteTitle || noteDesc) {
+        let dateObj = new Date(),
+            month = months[dateObj.getMonth()],
+            day = dateObj.getDate(),
+            year = dateObj.getFullYear();
+
+
+        let noteInfo = {
+            title: noteTitle, description: noteDesc,
+            date: `${month} ${day}, ${year}`
+        }
+        notes.push(noteInfo);
+        localStorage.setItem("notes", JSON.stringify(notes));
+        closeIcon.click();
+        showNotes();
+
+    }
+});
